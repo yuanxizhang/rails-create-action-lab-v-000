1373c7f777c43c3cd00cfb457a4694f822b4c79c
Yancy Zhang <yuanxi.zhang@gmail.com>
2019-03-08 19:25:33 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -12,6 +12,11 @@ class StudentsController < ApplicationController
   end
 
   def create
-  end
+	  @studnet = Student.new
+	  @student.first_name = params[:first_name]
+	  @student.last_name = params[:last_name]
+	  @student.save
+	  redirect_to post_path(@post)
+	end
 
 end
